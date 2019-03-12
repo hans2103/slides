04366d572840c0619e58e4c4d527055b32849d52
Hans Kuijpers <hans2103@gmail.com>
2019-03-12 08:47:19 +0100

    11-03-2019 jug073

--- public_html/index.php
+++ public_html/index.php
@@ -1,1 +1,6 @@
-<a href="20180807-jug073.html">Maak je website sneller - JUG073 07-08-2018</a>
+<ul>
+    <li><a href="20190311-jug073.html">RSForm!Pro - meer dan een contactformulier - JUG073 11-03-2019</a></li>
+    <li><a href="20190205-jug073.html">Template overrides - JUG073 05-02-2019</a></li>
+    <li><a href="20181008-jug073.html">Joomla Content Editor - JUG073 08-10-2018</a></li>
+    <li><a href="20180807-jug073.html">Maak je website sneller - JUG073 07-08-2018</a></li>
+</ul>
