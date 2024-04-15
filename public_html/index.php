46ca2001f48c2cd1150a68e0fd605633e416a04e
Hans Kuijpers <hans2103@gmail.com>
2024-04-15 16:17:08 +0200

    pending changes

--- public_html/index.php
+++ public_html/index.php
@@ -1,4 +1,5 @@
 <ul>
+    <li><a href="20230512-jdnl23.html">Layout Overrides in Child Themes - JDNL23 12-05-2023</a></li>
     <li><a href="20210705-jug040.html">Structured Data voor FAQ pagina - JUG040 5 juli 2021</a></li>
     <li><a href="20210607-jug040.html">RSForm!Pro submissions to Google Sheets - JUG040 07-06-2021</a></li>
     <li><a href="20210405-jug040.html">Google Analytics & Google Tag Manager - JUG040 05-04-2021</a></li>
